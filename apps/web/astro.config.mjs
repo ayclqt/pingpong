5b4cb3192fb41fa3bcd64031d00cd46a5eed51ef
ayclqt <quangtri.lam.9@gmail.com>
2026-04-16 17:36:45 +0700

    chore(config): Extract Github Pages site URLs from CI environment explicitly

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -4,10 +4,19 @@ import react from "@astrojs/react"
 import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "astro/config"
 
+const isGithubActions = process.env.GITHUB_ACTIONS === "true"
+const repoOwner = process.env.GITHUB_REPOSITORY_OWNER
+const repoName = process.env.GITHUB_REPOSITORY?.split("/")[1]
+
 // https://astro.build/config
 export default defineConfig({
+  site: isGithubActions ? `https://${repoOwner}.github.io` : undefined,
+  base: isGithubActions && repoName ? `/${repoName}` : undefined,
   vite: {
     plugins: [tailwindcss()],
+    css: {
+      transformer: "lightningcss",
+    },
   },
   integrations: [react()],
 })
